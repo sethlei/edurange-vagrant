98805dfac33e8bc2e2fc1c2285ea776b54d6a4a3
Seth Leichsenring <leichssw@gmail.com>
2022-06-18 18:13:24 -0700

    Initial Commit

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y git
+git clone -b autoinstall https://github.com/edurange/edurange-flask.git --recurse-submodules
+cd edurange-flask
+chmod +x install.sh
+useradd -m -s /bin/bash -g sudo -u 1001 -p "$(openssl passwd -1 edurange)" edurange
+sudo su edurange
+sudo ./install.sh auto
+npm start
