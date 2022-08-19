b872a60076314e61f2667fd9177018f2533f4671
Seth Leichsenring <leichssw@gmail.com>
2022-08-19 13:01:47 -0700

    Updated to test new branch name fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,13 @@
 
 echo ${GITHUB_REF##*/}
 
+echo ${BRANCH_NAME}
+
 apt-get update && apt-get upgrade -y
 apt-get install -y git
 useradd -m -s /bin/bash -g sudo -u 1001 -p "$(openssl passwd -1 edurange)" edurange
 sudo su edurange
-git clone https://github.com/edurange/edurange-flask.git --recurse-submodules
+git clone https://github.com/edurange/edurange-flask.git --recurse-submodules -b ${BRANCH_NAME}
 cd edurange-flask
 chmod +x install.sh
 sudo ./install.sh auto
